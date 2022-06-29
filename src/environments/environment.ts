f765394214a90e0687cf1349641f58da59631f63
Alex-Buttielie <63355403+Alex-Buttielie@users.noreply.github.com>
2022-06-29 10:59:31 -0300

    teste2
    teste2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   version: '1.0.0',
   URL: 'http://localhost:8085/'
 };
